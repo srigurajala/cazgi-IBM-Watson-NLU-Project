74c2f9bc7ac733829d864bf88e2123aa6e613455
Srinivas Gurajala <srinivas.gurajala@gmail.com>
2021-06-06 06:05:58 +0000

    Added NLU instance

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -1,6 +1,26 @@
 const express = require('express');
+const dotenv = require('dotenv');
 const app = new express();
+dotenv.config();
 
+function getNLUInstance() {
+    let api_key = process.env.API_KEY;
+    let api_url = process.env.API_URL;
+
+    const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+    const { IamAuthenticator } = require('ibm-watson/auth');
+
+    const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+        version: '2020-08-01',
+        authenticator: new IamAuthenticator({
+            apikey: '{apikey}',
+        }),
+        serviceUrl: '{url}',
+    });
+
+    return naturalLanguageUnderstanding;
+
+}
 app.use(express.static('client'))
 
 const cors_app = require('cors');
